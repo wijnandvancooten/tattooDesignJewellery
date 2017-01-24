22be59a9ffba3e0843ca7de8c7ddee41233fc6ec
wijnandvancooten <wijnandvancooten@hotmail.com>
2017-01-24 17:24:36 +0100

    base HTML/CSS ETC

---
+++ js/custom.js
@@ -0,0 +1,25 @@
+//general
+
+//index.html
+
+var navPositionY = $('.navbar').offset().top;
+
+
+function logScroll(){
+
+  
+  if ($(window).scrollTop() >= navPositionY){
+    
+    $('.navbar')
+      .css ('position','fixed')
+      .css('top','0')
+      .css ('left','0')
+      .css ('z-index', '3') 
+  }
+  else{
+     
+    $('.navbar')
+     .css ('position','relative')
+   
+  }
+}
---
+++ readme.txt
@@ -0,0 +1,1 @@
+persenal website 1.0
