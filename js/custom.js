2fc7dbcbb9d7f97fd9c088bba525e0f19f218219
wijnandvancooten <wijnandvancooten@hotmail.com>
2017-01-27 16:26:41 +0100

    nav-bar

--- js/custom.js
+++ js/custom.js
@@ -4,3 +4,10 @@ $( ".menu" ).hide();
 $( ".hamburger" ).click(function() {
 $( ".menu" ).slideToggle( "slow");
 });
+$(window).scroll(function() {
+
+    if ($(this).scrollTop()>0)
+     {
+        $('.menu').fadeOut();
+     }
+ });
