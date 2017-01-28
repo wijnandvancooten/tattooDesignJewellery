7c5c0faa9c1b7597a74e9235bdb6f2c0a5625836
Slickepinne <svenjaschaefer@gmail.com>
2017-01-28 19:18:47 +0100

    Shopping_Grid JS
    Add JS
    - problems with jQuery - wrong images shown

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,16 @@
 //custom.js for Tattoo Design webpage//
 
+/*** JS for Navigation ***/
 $( ".menu" ).hide();
 $( ".hamburger" ).click(function() {
 $( ".menu" ).slideToggle( "slow");
+});
+
+/*** JS for Shopping Grid ***/
+$(".shopping_list_item img").click(function(){
+  $(".preview").css({ 'display' : 'block' });
+});
+
+$(".preview_img").click(function(){
+  $(".preview").css({ 'display' : 'none' });
 });
