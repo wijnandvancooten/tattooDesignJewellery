6112cccfcb382f9b4adc29b57aa94306dbde07f2
wijnandvancooten <wijnandvancooten@hotmail.com>
2017-03-14 17:38:56 +0100

    start passport for login

--- app.js
+++ app.js
@@ -2,12 +2,31 @@
 const express = require('express'),
     bodyParser = require('body-parser'),
     pug = require('pug'),
-    sequelize = require('sequelize')
+    sequelize = require('sequelize'),
+    passport = require('passport'),
+    session = require('express-session')
 
 const app = express()
 //database parameters
 
-// set the public folder
+//API keys and Passport configuration.
+const passportConfig = require('./config/passport');
+//
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+//setting the session
+app.use(session({
+    secret: 'salad gelore',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure: false,
+        maxAge: 1000 * 60 * 60
+    }
+}))
+
+//setting the public folder
 app.use(express.static('public'))
 
 //sets the view engine to pug. pug renders the html page//
