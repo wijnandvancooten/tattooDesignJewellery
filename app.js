2494ea002f310de68ee786042077f3ab5c02303b
jeroenverhallen <jeroenjverhallen@gmail.com>
2017-03-14 17:39:43 +0100

    something was off

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const express = require('express'),
     sequelize = require('sequelize')
 
 const app = express()
-//database parameters
+//import database
+const db = require( __dirname + '/modules/db' )
+
+app.use( 'modules/db', db )
 
 // set the public folder
 app.use(express.static('public'))
@@ -17,46 +20,6 @@ app.set('view engine', 'pug')
 app.use( bodyParser.urlencoded( { extended:false} ) )
 
 // make the database
-const db = new sequelize( 'webshop', process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
-    host: 'localhost',
-    dialect: 'postgres'
-} )
-
-// define all the tables in the database
-const user = db.define( 'user', {
-    firstname: { type: sequelize.STRING, allowNull: false },
-    lastname: { type: sequelize.STRING, allowNull: false },
-    address: { type: sequelize.STRING, allowNull: false },
-    password: { type: sequelize.STRING, allowNull: false },
-    email: { type: sequelize.STRING, allowNull: false, isEmail: true  }
-} )
-
-const product = db.define( 'product', {
-    name: sequelize.STRING,
-    description: sequelize.STRING,
-    price: sequelize.FLOAT,
-    stock: sequelize.INTEGER,
-    image: sequelize.STRING
-} )
-
-const order = db.define( 'order', {
-    ordernumber: sequelize.INTEGER,
-    amount: sequelize.INTEGER,
-    totalprice: sequelize.FLOAT
-} )
-
-const comment = db.define( 'comment', {
-    name: { type: sequelize.STRING, allowNull: false },
-    content: { type: sequelize.STRING, allowNull: false }
-} )
-
-user.hasMany( order )
-order.belongsTo( user )
-order.hasMany( product )
-product.belongsTo( order )
-product.hasMany( comment )
-comment.belongsTo( product )
-
 app.get('/', (req, res) =>{
   res.render('application')
 })
@@ -64,27 +27,3 @@ app.get('/', (req, res) =>{
 app.listen(3000, function() {
     console.log('Web server started on port 3000. You rock!!!! ;).... Happy coding')
 })
-
-db.sync( { force: false} )
-.then( f => {
-    console.log( 'Table created!' )
-      return Promise.all( [
-				user.create( {
-	    		firstname: "Mike",
-                lastname: "Michaelson",
-                address: "some street 123",
-	    		password: "213jkdas8eqwa",
-	            email: "mike123@asd.com"
-				} ),
-				user.create( {
-					firstname: "Ernst",
-                    lastname: "Blofeld",
-                    address: "somewhere in a mountain",
-					password: "einfach",
-					email: "ernsteinfach@huhu.com"
-				})
-			])
-  })
-		.catch( err => {
-		    console.log('An error occured: ' + err)
-	     } )
