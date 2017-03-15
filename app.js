e8247de5f2529a6ae97546c967d7ecf81b7e00b0
jeroenverhallen <jeroenjverhallen@gmail.com>
2017-03-15 17:55:50 +0100

    everything is terrible

--- app.js
+++ app.js
@@ -3,15 +3,26 @@ const express = require('express'),
     bodyParser = require('body-parser'),
     pug = require('pug'),
     sequelize = require('sequelize')
+    session = require( 'express-session' )
 
 const app = express()
 //import database
 const db = require( __dirname + '/modules/db' )
 
-app.use( 'modules/db', db )
+
+//setting the session
+app.use( session( {
+    secret: 'salad gelore',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure: false,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+} ) )
 
 // set the public folder
-app.use(express.static('public'))
+app.use( express.static( 'public' ) )
 
 //sets the view engine to pug. pug renders the html page//
 app.set('view engine', 'pug')
@@ -23,6 +34,37 @@ app.use( bodyParser.urlencoded( { extended:false} ) )
 app.get('/', (req, res) =>{
   res.render('application')
 })
+
+app.post( '/application', ( req, res ) => {
+    console.log( 'someone is trying to buy something' )
+    if ( req.session.order ) {
+        console.log( 'the order in the current session is: ', req.session.order )
+        db.order.findOne( {
+            where: {
+                id: req.session.order.id
+            }, include : [
+                { model: db.product.findOne( {
+                    where: {
+                        name: req.params.name
+                    }
+                } ) }
+            ]
+        })
+    } else
+        console.log( 'let us get this order started' )
+        db.order.create( {
+            ordernumber: Math.floor((Math.random() * 9999) + 1000)
+        , include: [
+            { model: db.product.findOne( {
+                where: {
+                    name: req.params.name
+                }
+            } ) } 
+        ] } ).then( neworder => {
+            req.session.order = neworder
+            res.render( 'application', {order: neworder} )
+        } ).catch(console.log.bind(console))
+} )
 
 app.listen(3000, function() {
     console.log('Web server started on port 3000. You rock!!!! ;).... Happy coding')
--- modules/db.js
+++ modules/db.js
@@ -1,18 +1,16 @@
 //required packages for the app//
-const express = require('express'),
-    sequelize = require('sequelize')
+const express = sequelize = require('sequelize')
+let db = { }
 
-const router = express()
-//database parameters
 
 // make the database
-const db = new sequelize( 'webshop', process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
+db.conn = new sequelize( 'webshop', process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
     host: 'localhost',
     dialect: 'postgres'
 } )
 
 // define all the tables in the database
-const user = db.define( 'user', {
+db.user = db.conn.define( 'user', {
     firstname: { type: sequelize.STRING, allowNull: false },
     lastname: { type: sequelize.STRING, allowNull: false },
     address: { type: sequelize.STRING, allowNull: false },
@@ -20,7 +18,7 @@ const user = db.define( 'user', {
     email: { type: sequelize.STRING, allowNull: false, isEmail: true  }
 } )
 
-const product = db.define( 'product', {
+db.product = db.conn.define( 'product', {
     name: sequelize.STRING,
     description: sequelize.TEXT,
     price: sequelize.FLOAT,
@@ -28,86 +26,83 @@ const product = db.define( 'product', {
     image: sequelize.STRING
 } )
 
-const order = db.define( 'order', {
-    ordernumber: sequelize.INTEGER,
-    amount: sequelize.INTEGER,
-    totalprice: sequelize.FLOAT
+db.order = db.conn.define( 'order', {
+    ordernumber: sequelize.INTEGER
 } )
 
-const comment = db.define( 'comment', {
+db.comment = db.conn.define( 'comment', {
     name: { type: sequelize.STRING, allowNull: false },
     content: { type: sequelize.STRING, allowNull: false }
 } )
 
-user.hasMany( order )
-order.belongsTo( user )
-order.hasMany( product )
-product.belongsTo( order )
-product.hasMany( comment )
-comment.belongsTo( product )
+db.user.hasMany( db.order )
+db.order.belongsTo( db.user )
+db.order.hasMany( db.product )
+db.product.hasMany( db.comment )
+db.comment.belongsTo( db.product )
 
-db.sync( { force: true} )
+db.conn.sync( { force: true} )
 .then( f => {
     console.log( 'Table created!' )
-      return Promise.all( [
-		user.create( {
-	        firstname: "Mike",
-            lastname: "Michaelson",
-            address: "some street 123",
-	    	password: "213jkdas8eqwa",
-	        email: "mike123@asd.com"
-		} ),
-		user.create( {
-			firstname: "Ernst",
-            lastname: "Blofeld",
-            address: "somewhere in a mountain",
-			password: "einfach",
-			email: "ernsteinfach@huhu.com"
+        return Promise.all( [
+	        db.user.create( {
+	            firstname: "Mike",
+                lastname: "Michaelson",
+                address: "some street 123",
+	    	    password: "213jkdas8eqwa",
+	            email: "mike123@asd.com"
+		    } ),
+		    db.user.create( {
+			    firstname: "Ernst",
+                lastname: "Blofeld",
+                address: "somewhere in a mountain",
+			    password: "einfach",
+			    email: "ernsteinfach@huhu.com"
 			} )
 		] )
-  } ).then( f => {
-      product.create( {
+} ).then( f => {
+    db.product.create( {
         name: 'Secret Shadow',
         description: 'This substantial bracelet straps on with the force of a prison shackle. The only difference is you won&rsquo;t be fighting to take it off any time soon. There may be small differences between the pictured sterling silver piece and its stainless steel counterpart. The steel pieces may have slightly less detail than the handcrafted silver version.',
         price: 87.30,
         stock: 10,
         image: 'to be defined'
       } )
-  } ).then( f => {
-      product.create( {
+} ).then( f => {
+    db.product.create( {
         name: 'Silver Love',
         description: 'A collaboration with Chino at Black Widow Tattoo, this ring takes its name from Tartarus &mdash; the dark underworld of Greek mythology where souls were judged and the wicked received eternal punishment. There may be small differences between the pictured sterling silver piece and its stainless steel counterpart. The steel pieces may have slightly less detail than the handcrafted silver version.',
         price: 412.00,
         stock: 10,
         image: 'to be defined'
-      } )
-  } ).then( f => {
-      product.create( {
+    } )
+} ).then( f => {
+    db.product.create( {
         name: 'Sailors Heaven',
         description: 'The nice thing about wearing a chain with a big ol&rsquo; tomahawk on it is that it speaks for itself. Know what I mean? Comes on a 30-inch 3mm gauge stainless steel chain. Silver version has custom sterling silver hardware. There may be small differences between the pictured sterling silver piece and its stainless steel counterpart. The steel pieces may have slightly less detail than the handcrafted silver version.',
         price: 46.23,
         stock: 10,
         image: 'to be defined'
-      } )
-  } ).then( f => {
-      product.create( {
+    } )
+} ).then( f => {
+    db.product.create( {
         name: 'Miau',
         description: 'This substantial bracelet straps on with the force of a prison shackle. The only difference is you won&rsquo;t be fighting to take it off any time soon. There may be small differences between the pictured sterling silver piece and its stainless steel counterpart. The steel pieces may have slightly less detail than the handcrafted silver version.',
         price: 26.47,
         stock: 10,
         image: 'to be defined'
-      } )
-  } ).then( f => {
-      product.create( {
+    } )
+} ).then( f => {
+    db.product.create( {
         name: 'Lady in Black',
         description: 'Archers were a vital addition to any medieval army, and from high in the turrets, skilled bowmen were often the only hopes of holding off a violent usurping. This ring is for the sharpshooters working to prevent the siege. There may be small differences between the pictured sterling silver piece and its stainless steel counterpart. The steel pieces may have slightly less detail than the handcrafted silver version.',
         price: 10.99,
         stock: 10,
         image: 'to be defined'
-      } )
-  } )
+    } )
+} )
 	.catch( err => {
 	    console.log('An error occured: ' + err)
 	} )
 
-    module.exports = router
+    module.exports = db
